a3512ff80ce74a2bfcda5e94b16f379b1fcf0153
griguk77 <griguk77@gmail.com>
2023-08-06 18:04:42 +0300

    Added service

---
+++ app/src/main/java/ru/studyguk/servicestest/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.studyguk.servicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.studyguk.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
+    }
+}
