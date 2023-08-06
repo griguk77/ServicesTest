19c786176a660a16b785065c2c7f7c41b9cb9613
griguk77 <griguk77@gmail.com>
2023-08-07 02:37:20 +0300

    Added intent service

--- app/src/main/java/ru/studyguk/servicestest/MyIntentService.kt
+++ app/src/main/java/ru/studyguk/servicestest/MyIntentService.kt
@@ -1,5 +1,6 @@
 package ru.studyguk.servicestest
 
+import android.app.IntentService
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -11,13 +12,12 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.*
 
-class MyForegroundService: Service() {
-
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+class MyIntentService : IntentService(NAME) {
 
     override fun onCreate() {
         super.onCreate()
         log("onCreate")
+        setIntentRedelivery(false)
         createNotificationChannel()
         startForeground(NOTIFICATION_ID, createNotification())
     }
@@ -35,45 +35,38 @@ class MyForegroundService: Service() {
     }
 
     private fun createNotification() = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSubText("SubText")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
+        .setContentTitle("Title")
+        .setContentText("Text")
+        .setSubText("SubText")
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .build()
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 5) {
-                delay(1000)
-                log("Timer $i")
-            }
-            stopSelf()
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i")
         }
-        return START_STICKY
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
-        coroutineScope.cancel()
         log("onDestroy")
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
-    }
-
     private fun log(message: String) {
-        Log.d("RRR", "MyForegroundService $message")
+        Log.d("RRR", "MyIntentService $message")
     }
 
     companion object {
         private const val NOTIFICATION_ID = 1
         private const val CHANNEL_ID = "channel_id"
         private const val CHANNEL_NAME = "channel_name"
+        private const val NAME = "MyIntentService"
 
         fun newIntent(context: Context): Intent {
-            return Intent(context, MyForegroundService::class.java)
+            return Intent(context, MyIntentService::class.java)
         }
     }
 }
